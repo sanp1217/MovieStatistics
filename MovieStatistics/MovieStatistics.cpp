1f0068d99cc2078f43189f243d2a3864a53be738
Pablo <pablosgrad12@gmail.com>
2022-03-29 21:25:06 -0600

    Dynamically allocates array.

--- MovieStatistics/MovieStatistics.cpp
+++ MovieStatistics/MovieStatistics.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
-	
+	int studentsSurveyed, moviesSeen;
+	int* studentsArray = nullptr;
+
+	//input for students surveyed in order to dynamically allocate the amount into an array.
+	cout << "Number of students surveyed: ";
+	cin >> studentsSurveyed;
+
+	studentsArray = new int[studentsSurveyed];
 }
