f953fff61550c403de15c4945cbfc6c35e7bbbed
Pablo <pablosgrad12@gmail.com>
2022-03-30 16:27:19 -0600

    Creates function to find mode.

--- MovieStatistics/MovieStatistics.cpp
+++ MovieStatistics/MovieStatistics.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //function prototypes
 double calculateAverage(int*, int);
 double findMedian(int*, int);
+int findMode(int*, int);
 
 int main() {
 	int studentsSurveyed, moviesSeen;
@@ -48,4 +49,34 @@ double findMedian(int* studentsArray, int size) {
 	//if the size of the array is even, then the median will be the average of the two
 	//middle elements.
 	return (double)(studentsArray[(size - 1) / 2] + studentsArray[(size / 2)]) / 2.0;
+}
+
+int findMode(int* studentsArray, int size) {
+	//mode is the number that shows up most. modeCount is the number of times
+	//the mode number shows up.
+	int mode = 0, modeCount = 0;
+
+	for (int i = 0; i < size; i++) {
+		//The count variable is reset for each loop in the first for loop.
+		int count = 0;
+		//This second for loop compares one element to every other element in the array
+		//and counts the number of time the number shows up.
+		for (int j = 0; j < size; j++) {
+			if (*(studentsArray + j) == *(studentsArray + i)) {
+				count++;
+			}
+		}
+		//If the count of the numeber that just showed up is higher than the count of
+		//the mode then the count becomes the new modeCount and the mode is the number that was just looped.
+		if (count > modeCount) {
+			modeCount = count;
+			mode = *(studentsArray + i);
+		}
+	}
+	//If all of the numbers show up the same number of times then there is no mode.
+	if (modeCount == 1) {
+		mode = -1;
+	}
+
+	return mode;
 }
